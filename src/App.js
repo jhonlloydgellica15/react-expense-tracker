64ff342a4b38b323bf75a061bff2cdd70d4c8f91
jhonlloydgellica15 <jhonlloydgellica15@gmail.com>
2022-01-09 14:05:46 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is available</p>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
